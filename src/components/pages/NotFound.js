73d554bd07cb30e9004d5a99a353e8f5fcc1aeb5
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2019-11-11 01:35:48 +0600

    complete

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NorFound = () => {
+    return (
+    <div>
+		<h1>Not Found</h1>
+		<p className="lead">The page yor looking for does not exist...</p>
+    </div>
+    );
+};
+
+
+
+export default NorFound;
